580b4d3a6d2796b88b8e3a5a71447fa1b9e18b01
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-08-29 12:07:37 -0300

    feat: init configuration

---
+++ src/main/java/com/ferras/backendtransaction/BackEndTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.ferras.backendtransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndTransactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ferras/backendtransaction/BackEndTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferras.backendtransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
