fc67782a7faf9ff94e55fbd510b67c7ee826fc2b
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-08-29 12:30:05 -0300

    fix: TransactionRepository

--- src/main/java/com/ferras/backendtransaction/repository/TransactionRepository.java
+++ src/main/java/com/ferras/backendtransaction/repository/TransactionRepository.java
@@ -1,9 +1,10 @@
 package com.ferras.backendtransaction.repository;
 
+import com.ferras.backendtransaction.domain.transaction.Transaction;
 import com.ferras.backendtransaction.domain.user.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
 
-public interface TransactionRepository extends MongoRepository<User, Integer> {
+public interface TransactionRepository extends MongoRepository<Transaction, Integer> {
 }
