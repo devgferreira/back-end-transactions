be8bb58b2c16d88df16f40c924090cb7136cc383
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-08-31 10:54:10 -0300

    fix: creating methods in IUserService

--- src/main/java/com/ferras/backendtransaction/interfaces/IUserService.java
+++ src/main/java/com/ferras/backendtransaction/interfaces/IUserService.java
@@ -1,4 +1,9 @@
 package com.ferras.backendtransaction.interfaces;
+import com.ferras.backendtransaction.domain.user.User;
+import java.math.BigDecimal;
 
 public interface IUserService {
+    void validateTransaction(User sender, BigDecimal amount);
+    User findUserById(int id);
+    User saveUser(User user);
 }
