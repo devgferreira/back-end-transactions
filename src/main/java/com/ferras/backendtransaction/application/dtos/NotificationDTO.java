35c139d5cf4dc9aaf41d5c63d834acbcf00f037d
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-09-07 23:14:09 -0300

    feat: created NotificationDTO

---
+++ src/main/java/com/ferras/backendtransaction/application/dtos/NotificationDTO.java
@@ -0,0 +1,13 @@
+package com.ferras.backendtransaction.application.dtos;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class NotificationDTO {
+    String email;
+    String message;
+}
