3cb073685ffceea3d65bb5f9795563404ed7956f
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-09-07 23:16:25 -0300

    refactor: changing id implementation to String

--- src/main/java/com/ferras/backendtransaction/domain/model/user/User.java
+++ src/main/java/com/ferras/backendtransaction/domain/model/user/User.java
@@ -8,6 +8,8 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serial;
 import java.math.BigDecimal;
 
 
@@ -16,15 +18,17 @@ import java.math.BigDecimal;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User{
     @Id
-    private Long id;
+    private String id;
     private String firstName;
     private String lastName;
+
     @Indexed(unique = true)
     private String document;
     @Indexed(unique = true)
     private String email;
+
     private String password;
     private BigDecimal balance;
     private UserType userType;
