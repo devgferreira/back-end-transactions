50e7ec296bab7b4e513345a340dcf60740eb6d38
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-09-07 23:19:53 -0300

    feature: implementing the methods: createUser, getAllUsers and findUserByDocument

--- src/main/java/com/ferras/backendtransaction/application/service/UserService.java
+++ src/main/java/com/ferras/backendtransaction/application/service/UserService.java
@@ -1,24 +1,34 @@
 package com.ferras.backendtransaction.application.service;
 
+import com.ferras.backendtransaction.application.dtos.UserDTO;
 import com.ferras.backendtransaction.application.interfaces.IUserService;
 import com.ferras.backendtransaction.domain.enums.UserType;
 import com.ferras.backendtransaction.domain.repository.IUserRepository;
 import com.ferras.backendtransaction.domain.model.user.User;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 @Service
 public class UserService implements IUserService {
 
     private final IUserRepository _userRepository;
+    private final ModelMapper modelMapper;
     @Autowired
-    public UserService(IUserRepository userRepository) {
+    public UserService(IUserRepository userRepository, ModelMapper modelMapper) {
         _userRepository = userRepository;
+        this.modelMapper = modelMapper;
     }
 
     @Override
-    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+    public void validateTransaction(UserDTO senderDTO, BigDecimal amount) throws Exception {
+        User sender = modelMapper.map(senderDTO, User.class);
+
         if(sender.getUserType() == UserType.MERCHANT){
             throw new Exception("Usuário do tipo lojisa não está autorizado a realizar transação");
         }
@@ -28,12 +38,45 @@ public class UserService implements IUserService {
     }
 
     @Override
-    public User findUserById(Long id) throws Exception {
-        return  _userRepository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
+    public UserDTO findUserById(String id) throws Exception {
+        return modelMapper.map(_userRepository.findUserById(id).orElse(new User()), UserDTO.class);
     }
 
     @Override
-    public void saveUser(User user) {
-        _userRepository.save(user);
+    public void saveUser(UserDTO userDTO) {
+        User user = modelMapper.map(userDTO, User.class);
+        modelMapper.map(_userRepository.save(user), UserDTO.class);
+    }
+    @Override
+    public UserDTO createUser(UserDTO userDTO) throws Exception {
+        User user = modelMapper.map(userDTO, User.class);
+        if(_userRepository.findUserByDocument(userDTO.getDocument()).isPresent()){
+            throw new Exception("Cpf Duplicado");
+        }else{
+            return modelMapper.map(_userRepository.save(user), UserDTO.class);
+
+        }
+    }
+
+
+    @Override
+    public List<UserDTO> getAllUsers() {
+        List<User> users = _userRepository.findAll();
+        List<UserDTO> userDTOs = new ArrayList<>();
+        for (User user : users) {
+            UserDTO userDTO = modelMapper.map(user, UserDTO.class);
+            userDTOs.add(userDTO);
+        }
+        return userDTOs;
+    }
+
+    @Override
+    public UserDTO findUserByDocument(String document) throws Exception {
+        if(_userRepository.findUserByDocument(document).isPresent()){
+            return modelMapper.map(_userRepository.findUserByDocument(document).
+                    orElse(new User()), UserDTO.class);
+        }else{
+            throw new Exception("User not Found - Document:" + document);
+        }
     }
 }
