5cbe4ff822a185cce2f167a298f7000ee2bea3b0
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-09-08 22:37:28 -0300

    feature: created TransactionController

---
+++ src/main/java/com/ferras/backendtransaction/controlleres/TransactionController.java
@@ -0,0 +1,28 @@
+package com.ferras.backendtransaction.controlleres;
+
+import com.ferras.backendtransaction.application.dtos.TransactionDTO;
+import com.ferras.backendtransaction.application.interfaces.ITransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transaction")
+public class TransactionController {
+    private final ITransactionService _transactionService;
+
+    @Autowired
+    public TransactionController(ITransactionService transactionService) {
+        _transactionService = transactionService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TransactionDTO> createTransaction(@RequestBody TransactionDTO transactionDTO) throws Exception {
+        TransactionDTO newTransaction = _transactionService.createTransaction(transactionDTO);
+        return ResponseEntity.ok(newTransaction);
+    }
+
+}
